9abff37cdb9c7639b4a41b8bc4676d390ded67ca
kamva-hanisi <lucashanisi@gmail.com>
2023-03-17 02:22:02 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97;
+	int x = 65;
 
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (x <= 90)
+	{
+		putchar('\n');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
