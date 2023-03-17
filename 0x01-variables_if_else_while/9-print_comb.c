3686fafb32c079a7efa5cd7aef2e277637448a17
kamva-hanisi <lucashanisi@gmail.com>
2023-03-17 07:50:59 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,15 @@
 int main(void)
 {
 	int n;
-	int x;
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-	}
-	for (n = 97; x <= 102; x++)
-	{
-		putchar(x);
+		if (n == 57)
+		{
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
