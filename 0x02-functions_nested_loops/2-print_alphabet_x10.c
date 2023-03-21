a4641d6cc73b52e68b15e6980de65c9a1ba0b760
kamva-hanisi <lucashanisi@gmail.com>
2023-03-21 04:23:03 -0700

    repeat alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print alphabet_x10
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while(count < 10) {
+		char c;
+		for(c = 'a'; c <= 'z'; c++){
+			_putchar(c);
+        	}
+		_putchar('\n');
+		count++;
+	}
+}
