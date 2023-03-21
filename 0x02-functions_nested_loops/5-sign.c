942ec2556d9e1ff280fe11cc2ef03c4d2953cdee
kamva-hanisi <lucashanisi@gmail.com>
2023-03-21 07:51:51 -0700

    print numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_sign - print numbers
+ * @n: input int
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
