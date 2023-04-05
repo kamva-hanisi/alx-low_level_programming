089dea32bdb66a54289dd2bfd009ea530a665bed
kamva-hanisi <lucashanisi@gmail.com>
2023-04-05 04:57:28 -0700

    printing tasks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check(1, n));
+}
