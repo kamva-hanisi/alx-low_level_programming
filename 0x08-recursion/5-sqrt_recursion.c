83a320439a63291367600aa558c1a94e6599947f
kamva-hanisi <lucashanisi@gmail.com>
2023-04-05 09:37:22 -0700

    number is prime

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,20 @@
 #include "main.h"
+
+/**
+ * check - checks for the square root of a number
+ * @a:int
+ * @b:int
+ * Return: int
+ */
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
+}
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: integer to find sqrt of
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,21 @@
 #include "main.h"
+
+/**
+ * check - checks to see if number is prime
+ * @a:int
+ * @b:int
+ * Return:int
+ */
+int check(int a, int b)
+{
+	if (b < 2 || b % a == 0)
+		return (0);
+	else if (a > b / 2)
+		return (1);
+	else
+		return (check(a + 1, b));
+}
+
 /**
  * is_prime_number - states if number is prime
  * @n:int
