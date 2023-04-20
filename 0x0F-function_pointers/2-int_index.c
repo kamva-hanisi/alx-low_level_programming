e790f1a576dbb20f6dc89ac06be6b3be825071c2
kamva-hanisi <lucashanisi@gmail.com>
2023-04-20 04:19:55 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,21 +10,14 @@
   */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (size > 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
-
-				i++;
-			}
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
-
 	return (-1);
 }
