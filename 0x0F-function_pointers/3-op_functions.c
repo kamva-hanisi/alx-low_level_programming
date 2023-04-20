e98a55bde18b6603de9df5ea4b84b7ba7dc8398a
kamva-hanisi <lucashanisi@gmail.com>
2023-04-20 05:25:44 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
   * op_add - return the summ of two num
   * @a: first num
@@ -47,12 +53,6 @@ int op_mul(int a, int b)
   */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
@@ -64,12 +64,6 @@ int op_div(int a, int b)
   * Return: ...
   */
 int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
+}
 	return (a % b);
 }
