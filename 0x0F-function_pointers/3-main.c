3530d3300579271ff646041567304cbc76f0a7b2
kamva-hanisi <lucashanisi@gmail.com>
2023-04-20 06:05:49 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,10 +9,9 @@
   *
   * Return: ...
   */
-int main(int _attribute_((_unused_)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,22 +19,14 @@ int main(int _attribute_((_unused_)) argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3];
+	oprt = get_op_func(argv[2]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", get_op_func(op)(num1, num2));
-
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
