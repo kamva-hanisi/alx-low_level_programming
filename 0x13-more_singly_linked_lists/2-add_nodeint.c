6facee290670c2d5a6707a23e522ad7604fa106a
kamva-hanisi <lucashanisi@gmail.com>
2023-07-04 07:16:15 -0700

    new task

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,15 +9,15 @@
   */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *_cha;
+	listint_t *ptr;
 
 	if (head == NULL)
 		return (NULL);
-	cha = malloc(sizeof(listint_t));
-	if (cha == NULL)
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
 		return (NULL);
-	cha->n = n;
-	chr->next = *head;
-	*head = cha;
-	return (cha);
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
 }
