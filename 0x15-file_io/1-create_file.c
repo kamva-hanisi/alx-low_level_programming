e352f088a85239f79e3cfd7cbd71fdb0cfa78cc5
kamva-hanisi <lucashanisi@gmail.com>
2023-07-13 16:51:03 -0700

    add the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,19 +6,13 @@
  * @text_content: write string in the file
  * Return: -1
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int file_open, writter, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
-	}
-
-	for (len = 0; text_content[len];)
-	{
-		len++;
 	}
 
 	file_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -26,6 +20,14 @@ int create_file(const char *filename, char *text_content)
 	if (file_open == -1)
 	{
 		return (-1);
+	}
+
+	if (text_content != NULL) 
+	{
+		for (len = 0; text_content[len];)
+		{
+			len++;
+		}
 	}
 
 	writter = write(file_open, text_content, len);
