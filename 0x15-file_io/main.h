461088169f6117b4f23adc88240d4a00603ffb45
kamva-hanisi <lucashanisi@gmail.com>
2023-07-14 06:04:21 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,10 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv);
+void check100(int check, int file_open);
+void check97(int argc);
+void check98(ssize_t check, char *file, int file_open_from, int file_open_to);
+void check99(ssize_t check, char *file, int file_open_from, int file_open_to);
 
 #endif
