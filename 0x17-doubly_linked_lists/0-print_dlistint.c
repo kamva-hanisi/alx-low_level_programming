ece51a552d16089d8870da33abffb251a893bb4f
kamva-hanisi <lucashanisi@gmail.com>
2023-08-21 13:18:31 -0700

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,27 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
-* print_dlistint - Prints all elements of a distint_t list
-* @h: Pointer to the head of the list
-* Return: the number of elements
-*/
-size_t print_dlistint(const dlistint_t *h)
+ * add_dnodeint - add a new node at the beginning of the linked list
+ * @head: double pointer to the beginning of the linked list
+ * @n: value to add to the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t count = 0;
+	dlistint_t *new;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-
-	return (count);
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	if (new->next != NULL)
+		(new->next)->prev = new;
+	return (new);
 }
